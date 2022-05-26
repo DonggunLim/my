150829c3bf176b8eb7d132e61f9cdf546f8cc3a6
Ldonggun <ehdrjs130@gmail.com>
2022-05-26 20:54:12 +0900

    image 추가

---
+++ src/assets/image/lost99/index.ts
@@ -0,0 +1,4 @@
+import detail from './게시글상세페이지.png';
+import post from './게시글작성페이지.png';
+
+export { detail, post };
---
+++ src/assets/image/velog/index.ts
@@ -0,0 +1,4 @@
+import login from './벨로그로그인.png';
+import signup from './벨로그회원가입.png';
+
+export { login, signup };
